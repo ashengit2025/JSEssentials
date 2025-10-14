314c0262ddd17462dec0d2e5aa3382dd3b5a566d
ashengit2025 <amithavs@gmail.com>
2025-10-14 17:41:07 -0400

    Practice TASK- Product feedback using events in JS

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -7,6 +7,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const exp = document.getElementById('experince').value;
     
     alert('Thank oyu for your valuable feedback');
 
@@ -17,6 +18,7 @@ function submitFeedback() {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = exp;
 
     document.getElementById('userInfo').style.display = 'block';
 
